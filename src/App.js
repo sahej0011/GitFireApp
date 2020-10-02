bb66aa79c54faee79f830b83f24bc4b0d877eea2
Sahej Singh <sahejdeepsingh4@gmail.com>
2020-10-03 01:04:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import PageNotFound from "./Pages/PageNotFound";
+import { UserContext } from './Context/UserContext';
+import Footer from './Layout/Footer';
+import Header from './Layout/Header';
+import firebaseConfig from "./Config/FirebaseConfig";
+firebase.initializeApp(firebaseConfig);
+const  App = () => {
+  const [user,setUser] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer/>
+      <UserContext.Provider value={{user,setUser}}>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="*" component={PageNotFound} />
+        </Switch>
+        <Footer/>
+      </UserContext.Provider>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCxH0WhSH1p3jNSHE9UAwdgt5TkvXFFXnY",
+    authDomain: "mygitapp-5b274.firebaseapp.com",
+    databaseURL: "https://mygitapp-5b274.firebaseio.com",
+    projectId: "mygitapp-5b274",
+    storageBucket: "mygitapp-5b274.appspot.com",
+    messagingSenderId: "1057121378017",
+    appId: "1:1057121378017:web:d6ae44ef44920066ef61a0",
+    measurementId: "G-W5N2YX4CNE"
+};
